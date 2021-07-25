9ba48cb2a92bb7d7553fe184dd5d0b8fc472a269
juanescorreap <juanestebancope@outlook.com>
2021-07-25 17:21:03 -0500

    Implementation of printf

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * _printf - Function that  that prints anything.
+ * @format:  Character string.
+ * Return: The number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+
+}
