ba9517560cb58682b470f7e7a8434adf714db32d
juanescorreap <juanestebancope@outlook.com>
2021-07-28 18:25:20 -0500

    Documentation and style

--- holberton.h
+++ holberton.h
@@ -3,27 +3,33 @@
 #include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
-char * _itoa(int i, char *strout, int base);
-char *_utoa(unsigned i, char *strout, int base);
-char *_strrev (char *str);
+char *_itoa(int i, char *strout, int base);
+char *_utoa(unsigned int i, char *strout, int base);
+char *_strrev(char *str);
 char *string_tolower(char *tmp);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-void (*get_op_cases(char *format, int i))(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_d(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_s(va_list arguments, char *buffer, char *strtmp, char *tmp,  int *j);
-void _printf_percent(va_list arguments, char *buffer, char *strtmp, char *tmp, int *j);
-void _printf_char(va_list arguments, char *buffer, char *strtmp, char *tmp, int *j);
-void _printf_int(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_unsigned(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_octal(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_lowehexa(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_upperhexa(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-void _printf_address(va_list arguments, char *buffer, char *tmp, char *strtmp, int *j);
-
+void (*get_op_cases(char *, int))(va_list, char *, char *, char *, int *);
+void _printf_d(va_list, char *, char *, char *, int *);
+void _printf_s(va_list, char *, char *, char *,  int *);
+void _printf_percent(va_list, char *, char *, char *, int *);
+void _printf_char(va_list, char *, char *, char *, int *);
+void _printf_int(va_list, char *, char *, char *, int *);
+void _printf_unsigned(va_list, char *, char *, char *, int *);
+void _printf_octal(va_list, char *, char *, char *, int *);
+void _printf_lowehexa(va_list, char *, char *, char *, int *);
+void _printf_upperhexa(va_list, char *, char *, char *, int *);
+void _printf_address(va_list, char *, char *, char *, int *);
+/**
+ * options - struct with elements
+ * @cases: Pointer to char next to where % was found
+ * @function: Pointer to function that will be passed
+ * to perform printing
+ * Return: Void
+ */
 typedef struct options
 {
 char *cases;
-void (*function)(va_list arguments, char *buffer, char *tmp, char *strmp, int *j);
+void (*function)(va_list, char *, char *, char *, int *);
 } ops;
 #endif
