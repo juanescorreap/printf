4764f447525a6a421811cf25903701cd46267c87
juanescorreap <juanestebancope@outlook.com>
2021-07-28 18:51:25 -0500

    Documentation and style

--- string_tolower.c
+++ string_tolower.c
@@ -3,10 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+/**
+ * *string_tolower - Function that changes all
+ * uppercase letters of a string to lowecase
+ * Return: String of type char
+ * @tmp: String to be modified
+ */
 char *string_tolower(char *tmp)
 {
 	int a;
+
 	for (a = 0; tmp[a] != '\0'; a++)
 	{
 		if (tmp[a] >= 65 && tmp[a] <= 90)
