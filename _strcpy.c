ffc41bf4334468f680ed5053d50c2987e59afd87
3387-hub <3387@holbertonschool.com>
2021-07-29 04:46:49 +0000

    I fixed the difference problem.

--- _strcpy.c
+++ _strcpy.c
@@ -6,7 +6,7 @@
  *including the terminating null byte (\0), to the buffer pointed to by dest.
  * Return: Character copied from the first string to the second
  *@dest: Pointer to the string to be copied
- *@src: Pointer to the buffer where the string is to be pasted
+ *@src: Pointer to the buffer where the string is to be pasted.
  */
 char *_strcpy(char *dest, char *src)
 {
